5f64f407b9660085f7c39c9026ed5c21f7979a89
patryk <patrykoloss@gmail.com>
2023-01-07 17:06:14 +0100

    Add PlayerNotFoundException

---
+++ src/main/java/com/paszkowski/pokerek/exception/PlayerNotFoundException.java
@@ -0,0 +1,19 @@
+package com.paszkowski.pokerek.exception;
+
+public class PlayerNotFoundException extends Exception {
+    public PlayerNotFoundException() {
+        super();
+    }
+
+    public PlayerNotFoundException(String message) {
+        super(message);
+    }
+
+    public PlayerNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public PlayerNotFoundException(Throwable cause) {
+        super(cause);
+    }
+}
