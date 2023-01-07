1bff74dec7bf7e0d90316f8400b5513023218093
patryk <patrykoloss@gmail.com>
2023-01-07 17:05:38 +0100

    Initial commit

---
+++ src/main/java/com/paszkowski/pokerek/PokerekApplication.java
@@ -0,0 +1,13 @@
+package com.paszkowski.pokerek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokerekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokerekApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/paszkowski/pokerek/PokerekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paszkowski.pokerek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokerekApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
